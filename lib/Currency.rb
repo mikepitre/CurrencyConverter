223b75239a7bad3c52877fa52b6ab1a4348fac67
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-21 17:07:22 -0500

    Currency objects with same code can be added or subtracted. An error is raised when they have diff codes.

--- lib/Currency.rb
+++ lib/Currency.rb
@@ -1,3 +1,6 @@
+class DifferentCurrencyCode < StandardError
+end
+
 class Currency
   attr_accessor :amount, :code
 
@@ -10,7 +13,21 @@ class Currency
     other.is_a?(Currency) && (other.amount == self.amount) && (other.code == self.code)
   end
 
+  def +(other)
+    if other.is_a?(Currency) && (other.code == self.code)
+      @amount += other.amount
+    else
+      raise DifferentCurrencyCode
+    end
+  end
 
+  def -(other)
+    if other.is_a?(Currency) && (other.code == self.code)
+      @amount -= other.amount
+    else
+      raise DifferentCurrencyCode
+    end
+  end
 
 
 
--- spec/CurrencyTest.rb
+++ spec/CurrencyTest.rb
@@ -22,6 +22,25 @@ class CurrencyTest < Minitest::Test
     assert(object1 != object2)
   end
 
+  def test_objects_with_same_code_should_be_able_to_be_added
+    object1 = Currency.new(100, "AUD")
+    object2 = Currency.new(50, "AUD")
+    assert_equal(150, object1 + object2)
+  end
+
+  def test_objects_with_same_code_should_be_able_to_be_subtracted
+    object1 = Currency.new(100, "AUD")
+    object2 = Currency.new(50, "AUD")
+    assert_equal(50, object1 - object2)
+  end
+
+  def test_objects_with_different_currency_codes_raise_error_when_added_or_subtracted
+    object1 = Currency.new(100, "USD")
+    object2 = Currency.new(100, "AUD")
+    assert_raises(*DifferentCurrencyCode) {object1 + object2}
+  end
+
+
 
 
 
