7e20e3e776842d907b38ed278c5a2e47f8771fd7
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-22 14:54:50 -0500

    convert can exchange money between any known currencies, and made method round to 2 decial places

--- lib/CurrencyConverter.rb
+++ lib/CurrencyConverter.rb
@@ -7,13 +7,14 @@ class Currency_Converter
   attr_reader :rates
 
 def initialize
-  @rates = { "USD": 1.00, "AUD": 1.26685, "EUR": 0.90824 }
+  @rates = { "USD": 1.00, "AUD": 1.27941, "EUR": 0.90824 }
 end
 
 def convert(currency_object, new_currency_code)
   new_currency_code.to_s
   if (@rates.has_key?(new_currency_code)) && (@rates.has_key?(currency_object.code))
-    currency_object.amount * (@rates[new_currency_code]/@rates[currency_object.code])
+    result = currency_object.amount * (@rates[new_currency_code]/@rates[currency_object.code])
+    result.round(2)
   else
     raise UnknownCurrencyCodeError
   end
--- spec/CurrencyConverterTest.rb
+++ spec/CurrencyConverterTest.rb
@@ -5,11 +5,17 @@ require_relative "../lib/CurrencyConverter.rb"
 class CurrencyConverter < Minitest::Test
 
   def test_hash_created_with_rates
-    assert_equal({"USD": 1.00, "AUD": 1.26685, "EUR": 0.90824 }, Currency_Converter.new.rates)
+    assert_equal({"USD": 1.00, "AUD": 1.27941, "EUR": 0.90824 }, Currency_Converter.new.rates)
   end
 
   def test_currency_converter_works_with_known_currencies
     object1 = Currency.new(10, :USD)
-    assert_equal(12.6685, Currency_Converter.new.convert(object1, :AUD ))
+    assert_equal(12.79, Currency_Converter.new.convert(object1, :AUD ))
   end
+
+  def test_currecy_converter_works_between_any_currencies_it_knows
+    object1 = Currency.new(10, :AUD)
+    assert_equal(7.10, Currency_Converter.new.convert(object1, :EUR))
+  end
+
 end
